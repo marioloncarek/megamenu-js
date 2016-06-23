f3ffb7fcdb970919c1c6621dcb4fa21027ef7fa4
Mario Loncarek <mario.loncarek@gmail.com>
2016-06-23 15:18:23 +0200

    v2 23.06.2016

--- src/js/megamenu.js
+++ src/js/megamenu.js
@@ -2,6 +2,8 @@
 
     'use strict';
 
+    //get real window width for responsive
+
     function getWindowWidth() {
         var windowWidth = 0;
         if (typeof(window.innerWidth) === 'number') {
@@ -20,26 +22,21 @@
 
     $(document).ready(function () {
 
+        //vars
+
         var responsiveBreakpoint = 767;
 
-        $('.menu > ul').before('<button class="hamburger hamburger--squeeze" type="button"><span class="hamburger-menu-text">Navigation</span><span class="hamburger-box"><span class="hamburger-inner"></span></span></button>');
-
-        $('.hamburger').on('click', function () {
-            $(this).toggleClass('is-active');
-            $('.menu > ul').toggleClass('is-shown-on-mobile');
-        });
-
-        $('.menu > ul > li > ul:not(:has(ul))').addClass('is-classic-dropdown');
+        //menu
 
         $('.menu > ul > li').on({
             mouseenter: function () {
                 if (getWindowWidth() >= responsiveBreakpoint) {
-                    $(this).children('ul').css('display', 'block').removeClass('fadeOutUp').addClass('fadeInDown');
+                    $(this).children('ul').stop().css('z-index', 999).css('display', 'block').removeClass('animation-out').addClass('animation-in');
                 }
             },
             mouseleave: function () {
                 if (getWindowWidth() >= responsiveBreakpoint) {
-                    $(this).children('ul').removeClass('fadeInDown').addClass('fadeOutUp').delay(200).queue(function (next) {
+                    $(this).children('ul').stop().css('z-index', -10).removeClass('animation-in').addClass('animation-out').delay(200).queue(function (next) {
                         $(this).css('display', 'none');
                         next();
                     });
@@ -51,6 +48,39 @@
                 }
             }
         });
+
+        //classic menu
+
+        $('.menu > ul > li > ul:not(:has(ul))').addClass('is-classic-dropdown');
+
+        //dropdown indicator
+
+        $('.menu > ul > li:has(ul)').attr('aria-haspopup', 'true').append('<button class="dropdown-indicator" type="button">+</button>');
+
+        //mobile menu
+
+        $('.menu > ul').attr({
+            'role': 'menubar',
+            'aria-hidden': 'false'
+        }).before('<button class="hamburger hamburger--squeeze" type="button" aria-label="Menu" aria-controls="navigation"><span class="hamburger-menu-text">Navigation</span><span class="hamburger-box"><span class="hamburger-inner"></span></span></button>');
+
+        $('.hamburger').on('click', function () {
+            $(this).toggleClass('is-active');
+            $('.menu > ul').toggleClass('is-shown-on-mobile');
+        });
+
+        //aria
+
+        $('.menu li').attr('role', 'menuitem');
+
+        $('.menu li > a').attr('tabindex', 0);
+
+        $('.menu > ul > li > ul').attr({
+            'role': 'menu',
+            'aria-hidden': 'true',
+            'aria-label': 'submenu'
+        });
+
     });
 
     /*$(window).resize(function () {
